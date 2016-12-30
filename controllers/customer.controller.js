78ab85fb983430010a79b9512f2bac86e19ca409
Dimuthu de Silva <dimuthu.d@eblix.com.au>
2016-12-30 20:48:49 +0530

    changes

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -1,6 +1,21 @@
 angular.module('customer.list',[])
 .controller('CustomerCtrl',CustomerCtrl)
+.controller('CustomerCtrlNew',CustomerCtrlNew)
 .service('CustomerService',CustomerService);
+
+function CustomerService($http){
+
+    return {
+        getCustomers:getCustomers
+    };
+
+    function getCustomers(type){
+
+      return  $http.get("http://localhost:8000/customers?status="+type);
+
+    }
+}
+
 
 function CustomerCtrl(CustomerService){
     
@@ -24,15 +39,10 @@ function CustomerCtrl(CustomerService){
 }
 
 
-function CustomerService($http){
-
-    return {
-        getCustomers:getCustomers
-    };
-
-    function getCustomers(type){
-
-      return  $http.get("http://localhost:8000/customers?status="+type);
-
-    }
+function CustomerCtrlNew() {
+    var vm = this;
+    
+    vm.customer = {};
 }
+
+
